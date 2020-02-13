c23aa5e3bfc6b9c40261c9d6faf5a70b0f98c6ad
Timo Sturm <timo.sturm@netknights.it>
2020-02-13 11:55:30 +0100

    added license info

--- lib/utils/license_utils.dart
+++ lib/utils/license_utils.dart
@@ -40,6 +40,7 @@ addAllLicenses() {
   _addNewLicense("flutter_slidable", _FLUTTER_SLIDABLE_LICENSE);
   _addNewLicense("intl", _INTL_LICENSE);
   _addNewLicense("package_info", _PACKAGE_INFO);
+  _addNewLicense("dynamic_theme", _DYNAMIC_THEME_LICENSE);
 }
 
 _addNewLicense(String packageName, String licenseText) {
@@ -48,6 +49,29 @@ _addNewLicense(String packageName, String licenseText) {
   });
 }
 
+const String _DYNAMIC_THEME_LICENSE = '''
+MIT License
+
+Copyright (c) 2019 Norbert Kozsir
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+''';
 const String _PI_AUTHENTICATOR_LICENSE = '''
 
                                  Apache License
