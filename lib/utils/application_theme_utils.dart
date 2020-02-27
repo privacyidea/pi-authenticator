952fb95cbab2a975ad7a66743af79f9c4198ef49
Timo Sturm <timo.sturm@netknights.it>
2020-02-27 12:56:37 +0100

    Added some comments

--- lib/utils/application_theme_utils.dart
+++ lib/utils/application_theme_utils.dart
@@ -21,6 +21,9 @@
 import 'package:dynamic_theme/dynamic_theme.dart';
 import 'package:flutter/material.dart';
 
+/// Builds the theme of this application, the theme is dependent on the
+/// brightness of the device, when brightness == dark the dark theme of this
+/// app is returned, otherwise the light theme is returned.
 ThemeData getApplicationTheme(Brightness brightness) {
   bool isDark = brightness == Brightness.dark;
 
@@ -52,6 +55,9 @@ ThemeData getApplicationTheme(Brightness brightness) {
 TextStyle getDialogTextStyle(bool isDark) =>
     TextStyle(color: isDark ? Colors.white : Colors.black);
 
+/// This method returns the input color with a slightly lower alpha value if
+/// isDark == true, otherwise the method returns input. This is used to make
+/// colors less bright in dark mode of the app.
 Color getTonedColor(Color input, bool isDark) {
   double f = 0.8;
 
@@ -61,8 +67,13 @@ Color getTonedColor(Color input, bool isDark) {
       : input;
 }
 
+/// Evaluates if the application is in dark mode, that is the case if the dark
+/// theme is selected in the settings, or if the device enabled the system wide
+/// dark mode.
 bool isDarkModeOn(BuildContext context) =>
     DynamicTheme.of(context).brightness == Brightness.dark ||
     MediaQuery.of(context).platformBrightness == Brightness.dark;
 
+/// Default scale for Text() that is used inside AppBar() as title, this
+/// guarantees that the application name fits in the app bar.
 double get screenTitleScaleFactor => 0.95;
