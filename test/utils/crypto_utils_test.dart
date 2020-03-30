5e8863ea29b1e5193f171cd1495a5cbeaff78f8f
Timo Sturm <timo.sturm@netknights.it>
2020-03-30 10:57:28 +0200

    Added test for serializing rsa keys

--- test/utils/crypto_utils_test.dart
+++ test/utils/crypto_utils_test.dart
@@ -841,8 +841,8 @@ void _testPbkdf2() {
 }
 
 void _testRSASigning() {
-  group('rsa signing and verifying', () {
-    test('signature is valid', () async {
+  group('RSA signing and verifying', () {
+    test('Signature is valid', () async {
       var asymmetricKeyPair = await generateRSAKeyPair();
       RSAPublicKey publicKey = asymmetricKeyPair.publicKey;
       RSAPrivateKey privateKey = asymmetricKeyPair.privateKey;
@@ -855,7 +855,7 @@ void _testRSASigning() {
           true, validateSignature(publicKey, utf8.encode(message), signature));
     });
 
-    test('signature is invalid', () async {
+    test('Signature is invalid', () async {
       var asymmetricKeyPair = await generateRSAKeyPair();
       RSAPublicKey publicKey = asymmetricKeyPair.publicKey;
       RSAPrivateKey privateKey = asymmetricKeyPair.privateKey;
@@ -872,7 +872,7 @@ void _testRSASigning() {
               signature));
     });
 
-    test('signature is invalid because of flipped parameters', () async {
+    test('Signature is invalid because of flipped parameters', () async {
       var asymmetricKeyPair = await generateRSAKeyPair();
       RSAPublicKey publicKey = asymmetricKeyPair.publicKey;
       RSAPrivateKey privateKey = asymmetricKeyPair.privateKey;
@@ -888,75 +888,90 @@ void _testRSASigning() {
 }
 
 void _testSerializingRSAKeys() {
-  group('serialize rsa public keys', () {
-    test('Converting key', () async {
-      RSAPublicKey publicKey =
-          RSAPublicKey(BigInt.from(431254), BigInt.from(32545));
+  group("Serialize RSA public keys", () {
+    group('PKCS#1 format', () {
+      test('Converting key', () async {
+        RSAPublicKey publicKey =
+            RSAPublicKey(BigInt.from(431254), BigInt.from(32545));
 
-      String base64String = convertPublicKeyToDER(publicKey);
-      RSAPublicKey convertedKey = convertDERToPublicKey(base64String);
+        String base64String = convertPublicKeyToDER(publicKey);
+        RSAPublicKey convertedKey = convertDERToPublicKey(base64String);
 
-      expect(publicKey.modulus, convertedKey.modulus);
-      expect(publicKey.exponent, convertedKey.exponent);
+        expect(publicKey.modulus, convertedKey.modulus);
+        expect(publicKey.exponent, convertedKey.exponent);
+      });
+
+      test('Converting generated key', () async {
+        var asymmetricKeyPair = await generateRSAKeyPair();
+        RSAPublicKey publicKey = asymmetricKeyPair.publicKey;
+
+        String base64String = convertPublicKeyToDER(publicKey);
+        RSAPublicKey convertedKey = convertDERToPublicKey(base64String);
+
+        expect(publicKey.modulus, convertedKey.modulus);
+        expect(publicKey.exponent, convertedKey.exponent);
+      });
+
+      test('Parsing existing key', () async {
+        String serializedPublicKey = "MIICCgKCAgEAtOE6hDrwB+9Quk5Ibp9DduUMAmQ"
+            "i3KSn4pSZPrj4vhx9COenh+K6NtWFDwSPZcEOMk/s7GXsgAzdQvUVp4KpmBSAL3C"
+            "XgwZrhG4DZWRvXhB4P0Toxz1McVnPvabriWqU1L3Jorca1bnlvaaYh9rywbBrxes"
+            "IA4VUmfFoWHpn+HMdYp4g2UG1UeBIqBsgI4syPiwlEDW6sWTeSDcvQWTYGBsHMXf"
+            "zqNGT6ONo5mTSGqI7F75+KtJdtWfNxOKC9pKXXDG8UlgkkhWu0N6sCu/1PEsDxrc"
+            "pW7sKKrrB37J8jbEIOHzg67LgCWqFQMoBmIVRHlzQb5HKIswP10AmjJ7Mks0H1db"
+            "jK0/ONnU4A9QzjM0ZQt3mvCe8gE0FwQa7CYv8o1OKItQaxPhqBvcLJqjjXc8iFwJ"
+            "Qx5XsFU9jMJskQo+2pBBdW7oGRNqdyX0Zx36OQ48OaqbTciNT7oVQrIPd0oIiHjD"
+            "LnwBvwn3y5HmvmczdFAs2gQSryJ2/tS/zxrT/OjcGK4JQGDzbjog4fz7kox0PnGg"
+            "ssLfoonhflfpM5Om3vGePeqNnISTbA/yCH7X07dZf2BT5/41/OKzNjGzShFNwifb"
+            "WBf1mlwUNh1Vuu+ZGdTQKisxI4G8k2dZrlTWkQqOmLebCE3L38jnh0Oek+Jl9fNm"
+            "TcMl8sPWxB8lgGpUCAwEAAQ==";
+
+        expect(
+            convertPublicKeyToDER(convertDERToPublicKey(serializedPublicKey)),
+            serializedPublicKey);
+      });
     });
 
-    test('Converting generated key', () async {
-      var asymmetricKeyPair = await generateRSAKeyPair();
-      RSAPublicKey publicKey = asymmetricKeyPair.publicKey;
+    group('PKCS#8 format', () {
+      test('Converting key', () async {
+        RSAPublicKey publicKey =
+            RSAPublicKey(BigInt.from(431254), BigInt.from(32545));
 
-      String base64String = convertPublicKeyToDER(publicKey);
-      RSAPublicKey convertedKey = convertDERToPublicKey(base64String);
+        String base64String = derComplicatedToString(publicKey);
+        RSAPublicKey convertedKey = derComplicatedToKey(base64String);
 
-      expect(publicKey.modulus, convertedKey.modulus);
-      expect(publicKey.exponent, convertedKey.exponent);
+        expect(publicKey.modulus, convertedKey.modulus);
+        expect(publicKey.exponent, convertedKey.exponent);
+      });
+
+      test('Converting generated key', () async {
+        var asymmetricKeyPair = await generateRSAKeyPair();
+        RSAPublicKey publicKey = asymmetricKeyPair.publicKey;
+
+        String base64String = derComplicatedToString(publicKey);
+        RSAPublicKey convertedKey = derComplicatedToKey(base64String);
+
+        expect(publicKey.modulus, convertedKey.modulus);
+        expect(publicKey.exponent, convertedKey.exponent);
+      });
+
+      test('Parse existing key', () async {
+        String serializedPublicKey = "MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCA"
+            "gEAwdxugfnlsrd3rwZsEvI8GzEF4BtGEK3+vXRWVv43Z0Itn9NAtN5TWYgUkI/1RdI"
+            "ahWSZ8xM8vqza3Vb6SzI/vzw4O22TvFwNGDQcwIpxf/I0Iow+U/0uA0VFH2nPdyeJw"
+            "eNjEFaPkIZEHSyJ0CUtNS2umXpx4IyUN2R9Xve4OddbUpfTFPDYdcOiqPn1IkVLan/"
+            "t1fyEggabsk0Mdig+lK6JEd3keU1o9cOyHeiplOrmS5mNLV2Alz6Es+gvbvsMkXKvJ"
+            "rZ3+f8eVvRMNUgS/UfgIgPflUvUgxhlDCmCs/brZeZMhrUbWN00URdrfRT3xdSmNUV"
+            "10LPryk/l9quG8Phn8MKE1cKEEGWcBkuvF0v/f9DqMh6hsXea86oA//bYZM8Nb+mut"
+            "EjXSAi5AJxfryci0MGbL5jZaO8a2yfx41f84forxMReBCATDQIzSagMK9Ixln/h/U2"
+            "KZarenD6rB1rAd0pQLjXa9GMdfBJdImW3LYNpDaPuV/MPQOGRa851gCTf9Ha7rZl67"
+            "ekTgwlEAskZOp6NQz8ZdCl4oc7gaTGjFttBmH1TZtKtkpuvhqXv3Ige6XCzBH40+HC"
+            "nuwUCqJvPlKJHd/ikm2OfQS+BsPH8HDvrQGQyHyzBzV20oRfNGPIXVOXc9AEIJAPxB"
+            "QYQE2aoTR+l7N4On4x59z8qU1UCAwEAAQ==";
+
+        expect(derComplicatedToString(derComplicatedToKey(serializedPublicKey)),
+            serializedPublicKey);
+      });
     });
-
-    // TODO test with serialized key
-  });
-
-  group('Other format', () {
-    test('Converting key', () async {
-      RSAPublicKey publicKey =
-          RSAPublicKey(BigInt.from(431254), BigInt.from(32545));
-
-      String base64String = derComplicatedToString(publicKey);
-      RSAPublicKey convertedKey = derComplicatedToKey(base64String);
-
-      expect(publicKey.modulus, convertedKey.modulus);
-      expect(publicKey.exponent, convertedKey.exponent);
-    });
-
-    test('Converting generated key', () async {
-      var asymmetricKeyPair = await generateRSAKeyPair();
-      RSAPublicKey publicKey = asymmetricKeyPair.publicKey;
-
-      String base64String = derComplicatedToString(publicKey);
-      RSAPublicKey convertedKey = derComplicatedToKey(base64String);
-
-      expect(publicKey.modulus, convertedKey.modulus);
-      expect(publicKey.exponent, convertedKey.exponent);
-    });
-
-    test('parse working key', () async {
-      String serializedPublicKey = "MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCA"
-          "gEAwdxugfnlsrd3rwZsEvI8GzEF4BtGEK3+vXRWVv43Z0Itn9NAtN5TWYgUkI/1RdI"
-          "ahWSZ8xM8vqza3Vb6SzI/vzw4O22TvFwNGDQcwIpxf/I0Iow+U/0uA0VFH2nPdyeJw"
-          "eNjEFaPkIZEHSyJ0CUtNS2umXpx4IyUN2R9Xve4OddbUpfTFPDYdcOiqPn1IkVLan/"
-          "t1fyEggabsk0Mdig+lK6JEd3keU1o9cOyHeiplOrmS5mNLV2Alz6Es+gvbvsMkXKvJ"
-          "rZ3+f8eVvRMNUgS/UfgIgPflUvUgxhlDCmCs/brZeZMhrUbWN00URdrfRT3xdSmNUV"
-          "10LPryk/l9quG8Phn8MKE1cKEEGWcBkuvF0v/f9DqMh6hsXea86oA//bYZM8Nb+mut"
-          "EjXSAi5AJxfryci0MGbL5jZaO8a2yfx41f84forxMReBCATDQIzSagMK9Ixln/h/U2"
-          "KZarenD6rB1rAd0pQLjXa9GMdfBJdImW3LYNpDaPuV/MPQOGRa851gCTf9Ha7rZl67"
-          "ekTgwlEAskZOp6NQz8ZdCl4oc7gaTGjFttBmH1TZtKtkpuvhqXv3Ige6XCzBH40+HC"
-          "nuwUCqJvPlKJHd/ikm2OfQS+BsPH8HDvrQGQyHyzBzV20oRfNGPIXVOXc9AEIJAPxB"
-          "QYQE2aoTR+l7N4On4x59z8qU1UCAwEAAQ==";
-
-      expect(derComplicatedToString(derComplicatedToKey(serializedPublicKey)),
-          serializedPublicKey);
-    });
-  });
-
-  group('Formats should be interchangable', () {
-    // TODO
   });
 }
