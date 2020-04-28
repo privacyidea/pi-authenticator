4c619bca985b12b4493eabff4ae2191b08b7d314
Timo Sturm <timo.sturm@netknights.it>
2020-04-28 13:09:13 +0200

    moved file to right package

--- android/app/src/main/kotlin/it/netknights/piauthenticator/Application.kt
+++ android/app/src/main/kotlin/it/netknights/piauthenticator/Application.kt
@@ -1,4 +1,4 @@
-package com.example.privacyidea_authenticator
+package it.netknights.piauthenticator
 
 import io.flutter.app.FlutterApplication
 import io.flutter.plugin.common.PluginRegistry
@@ -8,12 +8,12 @@ import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService
 
 class Application : FlutterApplication(), PluginRegistrantCallback {
 
-  override fun onCreate() {
-    super.onCreate()
-    FlutterFirebaseMessagingService.setPluginRegistrant(this)
-  }
+    override fun onCreate() {
+        super.onCreate()
+        FlutterFirebaseMessagingService.setPluginRegistrant(this)
+    }
 
-  override fun registerWith(registry: PluginRegistry) {
-    GeneratedPluginRegistrant.registerWith(registry)
-  }
+    override fun registerWith(registry: PluginRegistry) {
+        GeneratedPluginRegistrant.registerWith(registry)
+    }
 }
