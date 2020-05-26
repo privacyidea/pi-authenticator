1918ce4d73d8d54853ecbb3fd94c78595eb703a6
Timo Sturm <timo.sturm@netknights.it>
2020-05-26 14:15:08 +0200

    Updated lib

--- local_plugins/firebase_messaging-6.0.12/lib/firebase_messaging.dart
+++ local_plugins/firebase_messaging-6.0.12/lib/firebase_messaging.dart
@@ -100,6 +100,8 @@ class FirebaseMessaging {
     return _iosSettingsStreamController.stream;
   }
 
+  /// Tells the plugin to use a firebase app with name [name] instead of the
+  /// default firebase app.
   void setApplicationName(String name) {
     _channel.invokeMethod("FcmSetApplicationName", name);
   }
