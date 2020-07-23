461d30123c2301ea29aa04a6e29d235a6f1ebd7f
Timo Sturm <timo.sturm@netknights.it>
2020-07-23 13:39:22 +0200

    Added custom text widget that automatically hides text

--- lib/widgets/custom_texts.dart
+++ lib/widgets/custom_texts.dart
@@ -1,3 +1,23 @@
+/*
+  privacyIDEA Authenticator
+
+  Authors: Timo Sturm <timo.sturm@netknights.it>
+
+  Copyright (c) 2017-2019 NetKnights GmbH
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
 import 'dart:async';
 
 import 'package:flutter/material.dart';
@@ -11,33 +31,34 @@ class HideableText extends StatefulWidget {
   final TextStyle textStyle;
   final bool enabled;
 
+  // TODO Change assertions!
   const HideableText(
       {Key key,
       this.text,
       this.hideOnDefault = true,
-      this.hiddenText = "",
+      this.hiddenText,
       this.textScaleFactor = 1.0,
       this.hideDuration,
-      this.textStyle, this.enabled = true})
+      this.textStyle,
+      this.enabled = true})
       : assert(text != null, "text is not an optional parameter."),
-        assert(hideDuration != null, "hideDuration is not an optional parameter."),
+        assert(
+            hideDuration != null, "hideDuration is not an optional parameter."),
         assert(textStyle != null, ""),
         super(key: key);
 
   @override
-  State<StatefulWidget> createState() => HideableTextState();
+  State<StatefulWidget> createState() =>
+      HideableTextState(isHidden: this.hideOnDefault);
 }
 
 class HideableTextState extends State<HideableText> {
   bool isHidden;
 
-  @override
-  void initState() {
-    isHidden = widget.hideOnDefault;
-    super.initState();
-  }
+  HideableTextState({bool isHidden}) : this.isHidden = isHidden;
 
   void showOTPValue() {
+    if (!widget.enabled || !isHidden) return;
     setState(() => isHidden = false);
     Timer(widget.hideDuration, () => setState(() => isHidden = true));
   }
@@ -46,7 +67,7 @@ class HideableTextState extends State<HideableText> {
   Widget build(BuildContext context) {
     return GestureDetector(
       child: Text(
-        isHidden ? widget.hiddenText : widget.text,
+        isHidden && widget.enabled ? widget.hiddenText : widget.text,
         textScaleFactor: widget.textScaleFactor,
         style: widget.textStyle,
       ),
