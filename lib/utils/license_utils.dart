e6e1eb3eb693f8f35c7a293fcb2ae76127445144
Timo Sturm <timo.sturm@netknights.it>
2020-07-24 09:57:13 +0200

    added and updated license note; remove unused file

--- lib/utils/license_utils.dart
+++ lib/utils/license_utils.dart
@@ -3,7 +3,7 @@
 
   Authors: Timo Sturm <timo.sturm@netknights.it>
 
-  Copyright (c) 2017-2019 NetKnights GmbH
+  Copyright (c) 2017-2020 NetKnights GmbH
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
