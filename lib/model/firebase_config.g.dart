2240303a3fe28a524aa9931e9bb333f0df57b33e
Timo Sturm <timo.sturm@netknights.it>
2020-09-29 12:02:35 +0200

    Generated serialization lib

--- lib/model/firebase_config.g.dart
+++ lib/model/firebase_config.g.dart
@@ -8,10 +8,11 @@ part of 'firebase_config.dart';
 
 FirebaseConfig _$FirebaseConfigFromJson(Map<String, dynamic> json) {
   return FirebaseConfig(
-      projectID: json['projectID'] as String,
-      projectNumber: json['projectNumber'] as String,
-      appID: json['appID'] as String,
-      apiKey: json['apiKey'] as String);
+    projectID: json['projectID'] as String,
+    projectNumber: json['projectNumber'] as String,
+    appID: json['appID'] as String,
+    apiKey: json['apiKey'] as String,
+  );
 }
 
 Map<String, dynamic> _$FirebaseConfigToJson(FirebaseConfig instance) =>
@@ -19,5 +20,5 @@ Map<String, dynamic> _$FirebaseConfigToJson(FirebaseConfig instance) =>
       'projectID': instance.projectID,
       'projectNumber': instance.projectNumber,
       'appID': instance.appID,
-      'apiKey': instance.apiKey
+      'apiKey': instance.apiKey,
     };
