8f7c5be5e4b83328ab5a293d89bcb2c84b53dc44
timosturm <54933730+timosturm@users.noreply.github.com>
2020-10-27 13:33:07 +0100

    Update lib/screens/main_screen.dart

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -89,7 +89,7 @@ class _MainScreenState extends State<MainScreen> {
           }
         },
         cancelOnError: false,
-        onError: (error) => print('$error'),
+        onError: (error) => log('$error', name: 'polling timer'),
       );
     });
 
