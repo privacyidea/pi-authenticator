4d188d6f89933d388f3656cd30a8d89774d371b8
Timo Sturm <timo.sturm@netknights.it>
2020-12-15 12:55:16 +0100

    Added CHANGELOG.md and show in app

--- lib/screens/update_screen.dart
+++ lib/screens/update_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 
 // TODO Check if the app was updated -> If yes, show this page, make accessible from settings?
@@ -8,6 +9,25 @@ import 'package:flutter_markdown/flutter_markdown.dart';
 class UpdateScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Markdown(data: 'Hallo');
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Changelog', // TODO Translate (?)
+            overflow: TextOverflow.ellipsis,
+            // maxLines: 2 only works like this.
+            maxLines: 2, // Title can be shown on small screens too.
+          ),
+        ),
+        body: FutureBuilder<String>(
+          future: DefaultAssetBundle.of(context).loadString('CHANGELOG.md'),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Markdown(
+                data: snapshot.data,
+              );
+            }
+            return CircularProgressIndicator();
+          },
+        ));
   }
 }
