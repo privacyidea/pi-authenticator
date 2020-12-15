d2626f39e1e725df27ca153cdeb38477700ce94c
Timo Sturm <timo.sturm@netknights.it>
2020-12-15 11:35:00 +0100

    Added placeholder for the update screen

---
+++ lib/screens/update_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+// TODO Check if the app was updated -> If yes, show this page, make accessible from settings?
+// TODO Show Information for each update version / changelog
+// TODO Format as Markdown?
+
+class UpdateScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
