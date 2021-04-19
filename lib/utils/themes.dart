16aafaa023848b293da3f9b5257539d5800e4b2c
Timo Sturm <timo.sturm@netknights.it>
2021-04-19 13:57:22 +0200

    Formated file

--- lib/utils/themes.dart
+++ lib/utils/themes.dart
@@ -1,13 +1,35 @@
+/*
+  privacyIDEA Authenticator
+
+  Authors: Timo Sturm <timo.sturm@netknights.it>
+
+  Copyright (c) 2017-2021 NetKnights GmbH
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
 import 'package:flutter/material.dart';
 
 var lightThemeData = new ThemeData(
-    primaryColor: Colors.blue,
-    textTheme: new TextTheme(button: TextStyle(color: Colors.white70)),
-    brightness: Brightness.light,
-    accentColor: Colors.blue);
+  primaryColor: Colors.blue,
+  textTheme: new TextTheme(button: TextStyle(color: Colors.white70)),
+  brightness: Brightness.light,
+  accentColor: Colors.blue,
+);
 
 var darkThemeData = ThemeData(
-    primaryColor: Colors.blue,
-    textTheme: new TextTheme(button: TextStyle(color: Colors.black54)),
-    brightness: Brightness.dark,
-    accentColor: Colors.blue);
+  primaryColor: Colors.blue,
+  textTheme: new TextTheme(button: TextStyle(color: Colors.black54)),
+  brightness: Brightness.dark,
+  accentColor: Colors.blue,
+);
