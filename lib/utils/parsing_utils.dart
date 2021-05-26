ebc2b4ed7cbd9ccce3716d405cdb5f1de05035c5
Timo Sturm <timo.sturm@netknights.it>
2021-05-26 12:47:45 +0200

    some parsing stuff

--- lib/utils/parsing_utils.dart
+++ lib/utils/parsing_utils.dart
@@ -266,7 +266,9 @@ Map<String, dynamic> parsePiAuth(Uri uri) {
         "[$pushVersionAsString] is not a valid value for parameter [v].");
   }
 
-  uriMap[URI_LABEL] = _parseLabel(uri);
+  List labelIssuerList = _parseLabelAndIssuer(uri);
+  uriMap[URI_LABEL] = labelIssuerList[0];
+  uriMap[URI_ISSUER] ??= labelIssuerList[1];
 
   uriMap[URI_SERIAL] = uri.queryParameters["serial"];
   ArgumentError.checkNotNull(uriMap[URI_SERIAL], "serial");
@@ -333,7 +335,9 @@ Map<String, dynamic> parseOtpAuth(Uri uri) {
     log("  $key | $value");
   });
 
-  uriMap[URI_LABEL] = _parseLabel(uri);
+  List labelIssuerList = _parseLabelAndIssuer(uri);
+  uriMap[URI_LABEL] = labelIssuerList[0];
+  uriMap[URI_ISSUER] ??= labelIssuerList[1];
 
   String algorithm = uri.queryParameters["algorithm"] ??
       enumAsString(Algorithms.SHA1); // Optional parameter
@@ -466,17 +470,26 @@ Map<String, dynamic> parseOtpAuth(Uri uri) {
   return uriMap;
 }
 
-String _parseLabel(Uri uri) {
-  String label;
+/// Parse the label and the issuer (if it exists) from the url.
+List _parseLabelAndIssuer(Uri uri) {
+  String label = "";
+  String issuer = "";
   String param = uri.path.substring(1);
+  param = Uri.decodeFull(param);
 
   try {
-    label = Uri.decodeFull(param);
+    if (param.contains(":")) {
+      List split = param.split(":");
+      issuer = split[0];
+      label = split[1];
+    } else {
+      label = param;
+    }
   } on Error {
     label = param;
   }
 
-  return label;
+  return [label, issuer];
 }
 
 String _parseIssuer(Uri uri) {
