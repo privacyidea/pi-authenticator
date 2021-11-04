4b09dbfed15fce2ed311fb1b15365d5c7aef485a
Timo Sturm <timo.sturm@netknights.it>
2021-11-04 11:42:53 +0100

    regenerated serialization methods

--- lib/model/tokens.g.dart
+++ lib/model/tokens.g.dart
@@ -6,19 +6,26 @@ part of 'tokens.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-HOTPToken _$HOTPTokenFromJson(Map<String, dynamic> json) => HOTPToken(
-      label: json['label'] as String,
-      issuer: json['issuer'] as String,
-      id: json['id'] as String,
-      algorithm: _$enumDecode(_$AlgorithmsEnumMap, json['algorithm']),
-      digits: json['digits'] as int,
-      secret: json['secret'] as String,
-      counter: json['counter'] as int? ?? 0,
-      isLocked: json['isLocked'] as bool? ?? false,
-      canToggleLock: json['canToggleLock'] as bool? ?? true,
-    )..type = json['type'] as String;
+HOTPToken _$HOTPTokenFromJson(Map<String, dynamic> json) {
+  return HOTPToken(
+    label: json['label'] as String,
+    issuer: json['issuer'] as String,
+    id: json['id'] as String,
+    algorithm: _$enumDecode(_$AlgorithmsEnumMap, json['algorithm']),
+    digits: json['digits'] as int,
+    secret: json['secret'] as String,
+    counter: json['counter'] as int,
+    isLocked: json['isLocked'] as bool,
+    canToggleLock: json['canToggleLock'] as bool,
+  )
+    ..avatarPath = json['avatarPath'] as String?
+    ..avatarColor = json['avatarColor'] as int?
+    ..type = json['type'] as String;
+}
 
 Map<String, dynamic> _$HOTPTokenToJson(HOTPToken instance) => <String, dynamic>{
+      'avatarPath': instance.avatarPath,
+      'avatarColor': instance.avatarColor,
       'canToggleLock': instance.canToggleLock,
       'isLocked': instance.isLocked,
       'type': instance.type,
@@ -63,19 +70,26 @@ const _$AlgorithmsEnumMap = {
   Algorithms.SHA512: 'SHA512',
 };
 
-TOTPToken _$TOTPTokenFromJson(Map<String, dynamic> json) => TOTPToken(
-      label: json['label'] as String,
-      issuer: json['issuer'] as String,
-      id: json['id'] as String,
-      algorithm: _$enumDecode(_$AlgorithmsEnumMap, json['algorithm']),
-      digits: json['digits'] as int,
-      secret: json['secret'] as String,
-      period: json['period'] as int,
-      isLocked: json['isLocked'] as bool? ?? false,
-      canToggleLock: json['canToggleLock'] as bool? ?? true,
-    )..type = json['type'] as String;
+TOTPToken _$TOTPTokenFromJson(Map<String, dynamic> json) {
+  return TOTPToken(
+    label: json['label'] as String,
+    issuer: json['issuer'] as String,
+    id: json['id'] as String,
+    algorithm: _$enumDecode(_$AlgorithmsEnumMap, json['algorithm']),
+    digits: json['digits'] as int,
+    secret: json['secret'] as String,
+    period: json['period'] as int,
+    isLocked: json['isLocked'] as bool,
+    canToggleLock: json['canToggleLock'] as bool,
+  )
+    ..avatarPath = json['avatarPath'] as String?
+    ..avatarColor = json['avatarColor'] as int?
+    ..type = json['type'] as String;
+}
 
 Map<String, dynamic> _$TOTPTokenToJson(TOTPToken instance) => <String, dynamic>{
+      'avatarPath': instance.avatarPath,
+      'avatarColor': instance.avatarColor,
       'canToggleLock': instance.canToggleLock,
       'isLocked': instance.isLocked,
       'type': instance.type,
@@ -88,29 +102,35 @@ Map<String, dynamic> _$TOTPTokenToJson(TOTPToken instance) => <String, dynamic>{
       'period': instance.period,
     };
 
-PushToken _$PushTokenFromJson(Map<String, dynamic> json) => PushToken(
-      label: json['label'] as String,
-      serial: json['serial'] as String,
-      issuer: json['issuer'] as String,
-      id: json['id'] as String,
-      isLocked: json['isLocked'] as bool? ?? false,
-      canToggleLock: json['canToggleLock'] as bool? ?? true,
-      sslVerify: json['sslVerify'] as bool?,
-      enrollmentCredentials: json['enrollmentCredentials'] as String?,
-      url: json['url'] == null ? null : Uri.parse(json['url'] as String),
-      expirationDate: DateTime.parse(json['expirationDate'] as String),
-    )
-      ..type = json['type'] as String
-      ..isRolledOut = json['isRolledOut'] as bool
-      ..publicServerKey = json['publicServerKey'] as String?
-      ..privateTokenKey = json['privateTokenKey'] as String?
-      ..publicTokenKey = json['publicTokenKey'] as String?
-      ..pushRequests = PushRequestQueue.fromJson(
-          json['pushRequests'] as Map<String, dynamic>)
-      ..knownPushRequests = CustomIntBuffer.fromJson(
-          json['knownPushRequests'] as Map<String, dynamic>);
+PushToken _$PushTokenFromJson(Map<String, dynamic> json) {
+  return PushToken(
+    label: json['label'] as String,
+    serial: json['serial'] as String,
+    issuer: json['issuer'] as String,
+    id: json['id'] as String,
+    isLocked: json['isLocked'] as bool,
+    canToggleLock: json['canToggleLock'] as bool,
+    sslVerify: json['sslVerify'] as bool?,
+    enrollmentCredentials: json['enrollmentCredentials'] as String?,
+    url: json['url'] == null ? null : Uri.parse(json['url'] as String),
+    expirationDate: DateTime.parse(json['expirationDate'] as String),
+  )
+    ..avatarPath = json['avatarPath'] as String?
+    ..avatarColor = json['avatarColor'] as int?
+    ..type = json['type'] as String
+    ..isRolledOut = json['isRolledOut'] as bool
+    ..publicServerKey = json['publicServerKey'] as String?
+    ..privateTokenKey = json['privateTokenKey'] as String?
+    ..publicTokenKey = json['publicTokenKey'] as String?
+    ..pushRequests =
+        PushRequestQueue.fromJson(json['pushRequests'] as Map<String, dynamic>)
+    ..knownPushRequests = CustomIntBuffer.fromJson(
+        json['knownPushRequests'] as Map<String, dynamic>);
+}
 
 Map<String, dynamic> _$PushTokenToJson(PushToken instance) => <String, dynamic>{
+      'avatarPath': instance.avatarPath,
+      'avatarColor': instance.avatarColor,
       'canToggleLock': instance.canToggleLock,
       'isLocked': instance.isLocked,
       'type': instance.type,
@@ -130,15 +150,17 @@ Map<String, dynamic> _$PushTokenToJson(PushToken instance) => <String, dynamic>{
       'knownPushRequests': instance.knownPushRequests,
     };
 
-PushRequest _$PushRequestFromJson(Map<String, dynamic> json) => PushRequest(
-      title: json['title'] as String,
-      question: json['question'] as String,
-      uri: Uri.parse(json['uri'] as String),
-      nonce: json['nonce'] as String,
-      sslVerify: json['sslVerify'] as bool,
-      id: json['id'] as int,
-      expirationDate: DateTime.parse(json['expirationDate'] as String),
-    );
+PushRequest _$PushRequestFromJson(Map<String, dynamic> json) {
+  return PushRequest(
+    title: json['title'] as String,
+    question: json['question'] as String,
+    uri: Uri.parse(json['uri'] as String),
+    nonce: json['nonce'] as String,
+    sslVerify: json['sslVerify'] as bool,
+    id: json['id'] as int,
+    expirationDate: DateTime.parse(json['expirationDate'] as String),
+  );
+}
 
 Map<String, dynamic> _$PushRequestToJson(PushRequest instance) =>
     <String, dynamic>{
@@ -151,11 +173,12 @@ Map<String, dynamic> _$PushRequestToJson(PushRequest instance) =>
       'title': instance.title,
     };
 
-PushRequestQueue _$PushRequestQueueFromJson(Map<String, dynamic> json) =>
-    PushRequestQueue()
-      ..list = (json['list'] as List<dynamic>)
-          .map((e) => PushRequest.fromJson(e as Map<String, dynamic>))
-          .toList();
+PushRequestQueue _$PushRequestQueueFromJson(Map<String, dynamic> json) {
+  return PushRequestQueue()
+    ..list = (json['list'] as List<dynamic>)
+        .map((e) => PushRequest.fromJson(e as Map<String, dynamic>))
+        .toList();
+}
 
 Map<String, dynamic> _$PushRequestQueueToJson(PushRequestQueue instance) =>
     <String, dynamic>{
@@ -163,11 +186,12 @@ Map<String, dynamic> _$PushRequestQueueToJson(PushRequestQueue instance) =>
     };
 
 SerializableRSAPublicKey _$SerializableRSAPublicKeyFromJson(
-        Map<String, dynamic> json) =>
-    SerializableRSAPublicKey(
-      BigInt.parse(json['modulus'] as String),
-      BigInt.parse(json['exponent'] as String),
-    );
+    Map<String, dynamic> json) {
+  return SerializableRSAPublicKey(
+    BigInt.parse(json['modulus'] as String),
+    BigInt.parse(json['exponent'] as String),
+  );
+}
 
 Map<String, dynamic> _$SerializableRSAPublicKeyToJson(
         SerializableRSAPublicKey instance) =>
@@ -177,13 +201,14 @@ Map<String, dynamic> _$SerializableRSAPublicKeyToJson(
     };
 
 SerializableRSAPrivateKey _$SerializableRSAPrivateKeyFromJson(
-        Map<String, dynamic> json) =>
-    SerializableRSAPrivateKey(
-      BigInt.parse(json['modulus'] as String),
-      BigInt.parse(json['exponent'] as String),
-      BigInt.parse(json['p'] as String),
-      BigInt.parse(json['q'] as String),
-    );
+    Map<String, dynamic> json) {
+  return SerializableRSAPrivateKey(
+    BigInt.parse(json['modulus'] as String),
+    BigInt.parse(json['exponent'] as String),
+    BigInt.parse(json['p'] as String),
+    BigInt.parse(json['q'] as String),
+  );
+}
 
 Map<String, dynamic> _$SerializableRSAPrivateKeyToJson(
         SerializableRSAPrivateKey instance) =>
@@ -194,9 +219,10 @@ Map<String, dynamic> _$SerializableRSAPrivateKeyToJson(
       'q': instance.q?.toString(),
     };
 
-CustomIntBuffer _$CustomIntBufferFromJson(Map<String, dynamic> json) =>
-    CustomIntBuffer()
-      ..list = (json['list'] as List<dynamic>).map((e) => e as int).toList();
+CustomIntBuffer _$CustomIntBufferFromJson(Map<String, dynamic> json) {
+  return CustomIntBuffer()
+    ..list = (json['list'] as List<dynamic>).map((e) => e as int).toList();
+}
 
 Map<String, dynamic> _$CustomIntBufferToJson(CustomIntBuffer instance) =>
     <String, dynamic>{
