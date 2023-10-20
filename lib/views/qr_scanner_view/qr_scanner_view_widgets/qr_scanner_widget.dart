9465990d5e4bfcef40e686dab9f7b155151824fb
Frank Merkel <138444693+frankmer@users.noreply.github.com>
2023-10-20 16:20:42 +0200

    new package works great

--- lib/views/qr_scanner_view/qr_scanner_view_widgets/qr_scanner_widget.dart
+++ lib/views/qr_scanner_view/qr_scanner_view_widgets/qr_scanner_widget.dart
@@ -1,51 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:qr_mobile_vision/qr_camera.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
-import '../../../utils/logger.dart';
-import '../../../utils/view_utils.dart';
 import 'qr_code_scanner_overlay.dart';
 
-class QRScannerWidget extends StatelessWidget {
-  final _key = GlobalKey<QrCameraState>();
-  QRScannerWidget({super.key});
+class QRScannerWidget extends StatefulWidget {
+  // final _key = GlobalKey<QrCameraState>();
+  const QRScannerWidget({super.key});
 
+  @override
+  State<QRScannerWidget> createState() => _QRScannerWidgetState();
+}
+
+class _QRScannerWidgetState extends State<QRScannerWidget> {
+  bool alreadyDetected = false;
   @override
   Widget build(BuildContext context) => SizedBox.expand(
         child: Stack(
           alignment: Alignment.center,
           children: [
-            QrCamera(
-              fit: BoxFit.cover,
-              key: _key,
-              formats: const [BarcodeFormats.QR_CODE],
-              // Ignore other codes than qr codes
-              onError: (context, e) {
-                WidgetsBinding.instance.addPostFrameCallback((_) {
-                  if (e is PlatformException && e.message == 'noPermission') {
-                    Logger.warning(
-                      'QRScannerView: Camera permission not granted.',
-                      name: 'QRScannerView#build#onError',
-                      error: e,
-                      stackTrace: StackTrace.current,
-                    );
-                    showMessage(message: 'Please grant camera permission to use the QR scanner.');
-                  }
-                  //   Navigator.pop(context, null);
-                  // _key.currentState!.stop();
+            MobileScanner(
+              fit: BoxFit.contain,
+              controller: MobileScannerController(
+                // facing: CameraFacing.back,
+                // torchEnabled: false,
+                returnImage: false,
+              ),
+              onDetect: (capture) {
+                if (alreadyDetected) return;
+                alreadyDetected = true;
+                final List<Barcode> barcodes = capture.barcodes;
+                for (final barcode in barcodes) {
+                  debugPrint('Barcode found! ${barcode.rawValue}');
+                }
 
-                  // Method must return a widget, so return one that does not display anything.
-                });
-                return const SizedBox();
-              },
-              // We have nothing to display in these cases, overwrite default
-              // behaviour with 'non-visible' content.
-              child: const SizedBox(),
-              notStartedBuilder: (_) => const SizedBox(),
-              offscreenBuilder: (_) => const SizedBox(),
-              qrCodeCallback: (code) {
-                Navigator.pop(context, code);
-                _key.currentState!.stop();
+                Navigator.pop(context, barcodes.first.rawValue);
               },
             ),
             Container(
