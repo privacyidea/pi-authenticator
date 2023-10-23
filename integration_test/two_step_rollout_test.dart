20b39b5204b106566beae5f7ba49bf07ad90445f
Frank Merkel <138444693+frankmer@users.noreply.github.com>
2023-10-23 10:10:27 +0200

    .

---
+++ integration_test/two_step_rollout_test.dart
@@ -0,0 +1,45 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:privacyidea_authenticator/main_netknights.dart';
+import 'package:privacyidea_authenticator/model/states/settings_state.dart';
+import 'package:privacyidea_authenticator/state_notifiers/settings_notifier.dart';
+import 'package:privacyidea_authenticator/state_notifiers/token_folder_notifier.dart';
+import 'package:privacyidea_authenticator/state_notifiers/token_notifier.dart';
+import 'package:privacyidea_authenticator/utils/app_customizer.dart';
+import 'package:privacyidea_authenticator/utils/riverpod_providers.dart';
+
+import '../test/tests_app_wrapper.dart';
+import '../test/tests_app_wrapper.mocks.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  late final MockSettingsRepository mockSettingsRepository;
+  late final MockTokenRepository mockTokenRepository;
+  late final MockTokenFolderRepository mockTokenFolderRepository;
+  setUp(() {
+    mockSettingsRepository = MockSettingsRepository();
+    when(mockSettingsRepository.loadSettings()).thenAnswer((_) async => SettingsState());
+    when(mockSettingsRepository.saveSettings(any)).thenAnswer((_) async => true);
+    mockTokenRepository = MockTokenRepository();
+    when(mockTokenRepository.loadTokens()).thenAnswer((_) async => []);
+    when(mockTokenRepository.saveOrReplaceTokens(any)).thenAnswer((_) async => []);
+    when(mockTokenRepository.deleteTokens(any)).thenAnswer((_) async => []);
+    mockTokenFolderRepository = MockTokenFolderRepository();
+    when(mockTokenFolderRepository.loadFolders()).thenAnswer((_) async => []);
+    when(mockTokenFolderRepository.saveOrReplaceFolders(any)).thenAnswer((_) async => []);
+  });
+  testWidgets(
+    '2step rollout test',
+    (tester) async {
+      await tester.pumpWidget(TestsAppWrapper(
+        overrides: [
+          settingsProvider.overrideWith((ref) => SettingsNotifier(repository: mockSettingsRepository)),
+          tokenProvider.overrideWith((ref) => TokenNotifier(repository: mockTokenRepository)),
+          tokenFolderProvider.overrideWith((ref) => TokenFolderNotifier(repository: mockTokenFolderRepository)),
+        ],
+        child: PrivacyIDEAAuthenticator(customization: ApplicationCustomization.defaultCustomization),
+      ));
+    },
+  );
+}
