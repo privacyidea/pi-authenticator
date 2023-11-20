0bc8c1c679346a896ed3d25ba220781d0da88572
Frank Merkel <138444693+frankmer@users.noreply.github.com>
2023-11-20 13:10:27 +0100

    .

--- lib/model/scheme_processors/implementations/otp_auth_migration_processor.dart
+++ lib/model/scheme_processors/implementations/otp_auth_migration_processor.dart
@@ -1,10 +1,5 @@
-// Original License:
-// The MIT License (MIT) Copyright © 2022 John R. Callahan
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
-// Code modified by Frank Merkel <frank.merkel@netknights.it>
+// Original Code: https://github.com/johncallahan/otpauth_migration/blob/main/lib/otpauth_migration.dart Copyright © 2022 John R. Callahan
+// Modified by Frank Merkel <frank.merkel@netknights.it>
 
 import 'otp_auth_processor.dart';
 import '../../tokens/token.dart';
