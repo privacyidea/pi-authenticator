c253e4c06cafae72a5eb962f6eb95f7da206b974
Frank Merkel <138444693+frankmer@users.noreply.github.com>
2024-01-03 14:44:45 +0100

    added home_screen widget iOS

---
+++ ios/single_token_widget/single_token_widgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  single_token_widgetBundle.swift
+//  single_token_widget
+//
+//  Created by Frank Merkel on 03.01.24.
+//  Copyright © 2024 The Chromium Authors. All rights reserved.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct single_token_widgetBundle: WidgetBundle {
+    var body: some Widget {
+        single_token_widget()
+    }
+}
