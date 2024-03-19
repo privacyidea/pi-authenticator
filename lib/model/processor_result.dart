1df2323801666cb40fb1776c77368daab2dbff0d
Frank Merkel <138444693+frankmer@users.noreply.github.com>
2024-03-19 15:29:03 +0100

    import tokens from freeotp+

--- lib/model/processor_result.dart
+++ lib/model/processor_result.dart
@@ -13,4 +13,16 @@ class ProcessorResult<T> {
   String toString() {
     return 'ProcessorResult(data: $data, success: $success, error: $error)';
   }
+
+  ProcessorResult<T> copyWith({
+    T? data,
+    bool? success,
+    String? error,
+  }) {
+    return ProcessorResult<T>(
+      data: data ?? this.data,
+      success: success ?? this.success,
+      error: error ?? this.error,
+    );
+  }
 }
