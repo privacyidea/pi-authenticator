8af32c037623740e433460eff72d1ad6fb950288
Frank Merkel <138444693+frankmer@users.noreply.github.com>
2024-03-28 09:40:42 +0100

    .

--- lib/utils/lock_auth.dart
+++ lib/utils/lock_auth.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:local_auth/local_auth.dart';
 import 'package:local_auth_android/local_auth_android.dart';
-import 'package:local_auth_ios/local_auth_ios.dart';
+import 'package:local_auth_darwin/local_auth_darwin.dart';
 
 import '../l10n/app_localizations.dart';
 import '../widgets/dialog_widgets/default_dialog.dart';
