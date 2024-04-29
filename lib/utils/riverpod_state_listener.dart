182664acc672daa4929e8dd17b783ed1dd768c7a
Frank Merkel <138444693+frankmer@users.noreply.github.com>
2024-04-29 10:08:48 +0200

    added deeplink notifier test

--- lib/utils/riverpod_state_listener.dart
+++ lib/utils/riverpod_state_listener.dart
@@ -73,4 +73,13 @@ class DeepLink {
   final Uri uri;
   final bool fromInit;
   const DeepLink(this.uri, {this.fromInit = false});
+
+  @override
+  bool operator ==(Object other) => other is DeepLink && other.uri == uri && other.fromInit == fromInit;
+
+  @override
+  int get hashCode => Object.hash(uri, fromInit);
+
+  @override
+  String toString() => 'DeepLink(uri: $uri, fromInit: $fromInit)';
 }
