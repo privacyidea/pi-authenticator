5d924ac1d3741e8dc40792983f04402efdffcc8c
Frank Merkel <138444693+frankmer@users.noreply.github.com>
2024-05-08 15:16:11 +0200

    added PushRequestRepository comments

--- lib/interfaces/repo/push_request_repository.dart
+++ lib/interfaces/repo/push_request_repository.dart
@@ -2,10 +2,18 @@ import '../../model/push_request.dart' show PushRequest;
 import '../../model/states/push_request_state.dart' show PushRequestState;
 
 abstract class PushRequestRepository {
+  /// Load the [PushRequestState] from the repository
   Future<PushRequestState> loadState();
+
+  /// Save the [PushRequestState] to the repository
   Future<void> saveState(PushRequestState pushRequestState);
+
+  /// Delete all [PushRequest]s from the [PushRequestState]
   Future<void> clearState();
 
+  /// Add a [PushRequest] to the [PushRequestState]
   Future<PushRequestState> add(PushRequest pushRequest, {PushRequestState? state});
+
+  /// Remove a [PushRequest] from the [PushRequestState]
   Future<PushRequestState> remove(PushRequest pushRequest, {PushRequestState? state});
 }
