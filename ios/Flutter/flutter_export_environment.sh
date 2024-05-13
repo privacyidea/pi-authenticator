ea43c8b6f31fe3a7ac7ab38d002e77dde075c79f
Frank Merkel <138444693+frankmer@users.noreply.github.com>
2024-05-13 15:18:59 +0200

    debug changes

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Frank Merkel\Documents\GitHub\pi-authenticator"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Frank Merkel\projects\authenticator_customizer\pi-authenticator"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
