1661419199557efec891f0e9dce0bc4f38c36fa3
Frank Merkel <138444693+frankmer@users.noreply.github.com>
2025-02-07 10:53:36 +0100

    Update copyright year to 2025 in multiple files

--- lib/model/enums/push_token_rollout_state.dart
+++ lib/model/enums/push_token_rollout_state.dart
@@ -3,7 +3,7 @@
  *
  * Author: Frank Merkel <frank.merkel@netknights.it>
  *
- * Copyright (c) 2024 NetKnights GmbH
+ * Copyright (c) 2025 NetKnights GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the 'License');
  * you may not use this file except in compliance with the License.
@@ -22,6 +22,8 @@ enum PushTokenRollOutState {
   rolloutNotStarted,
   generatingRSAKeyPair,
   generatingRSAKeyPairFailed,
+  receivingFirebaseToken,
+  receivingFirebaseTokenFailed,
   sendRSAPublicKey,
   sendRSAPublicKeyFailed,
   parsingResponse,
