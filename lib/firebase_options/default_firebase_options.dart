c089abe7b74e188acf3808bc96d74fecf6aa12a5
Frank Merkel <138444693+frankmer@users.noreply.github.com>
2025-07-28 17:30:19 +0200

    refactor: update Firebase options import and usage in main_netknights.dart

--- lib/firebase_options/default_firebase_options.dart
+++ lib/firebase_options/default_firebase_options.dart
@@ -21,6 +21,7 @@
 // ignore_for_file: type=lint
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:privacyidea_authenticator/firebase_options/netknights_firebase_options.dart';
 
 // import 'netknights_firebase_options.dart';
 
@@ -36,7 +37,7 @@ import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb, Tar
 /// ```
 class DefaultFirebaseOptions {
   static FirebaseOptions currentPlatformOf(String? app) => switch (app) {
-        // 'netknights' => NetknightsFirebaseOptions.currentPlatform,
+        'netknights' => NetknightsFirebaseOptions.currentPlatform,
         _ => defaultCurrentPlatform,
       };
   static FirebaseOptions get defaultCurrentPlatform {
